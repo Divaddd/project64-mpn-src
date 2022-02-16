c9b2b02eb32568beb4467e21f520184bff4d046c
Nora <norahanegan@gmail.com>
2022-02-16 16:32:46 -0500

    Update Discord Link

--- Source/Project64/UserInterface/MainMenuClass.cpp
+++ Source/Project64/UserInterface/MainMenuClass.cpp
@@ -525,7 +525,7 @@ bool CMainMenu::ProcessMessage(HWND hWnd, DWORD /*FromAccelerator*/, DWORD MenuI
     case ID_HELP_ABOUT: m_Gui->AboutBox(); break;
     case ID_HELP_ABOUTSETTINGFILES: m_Gui->AboutIniBox(); break;
     case ID_NETPLAY_REPLACESAVES: ShellExecute(NULL, "open", "Replace.bat", NULL, NULL, SW_SHOWMAXIMIZED); break;
-    case ID_NETPLAY_MPN: ShellExecute(NULL, "open", "https://discord.gg/0qi7VWGokA2Ffr4q", NULL, NULL, SW_SHOWMAXIMIZED); break;
+    case ID_NETPLAY_MPN: ShellExecute(NULL, "open", "https://discord.gg/marioparty", NULL, NULL, SW_SHOWMAXIMIZED); break;
     case ID_NETPLAY_UPDATE_EMULATOR:
         if (!g_Settings->LoadBool(GameRunning_CPU_Running))
 	         ShellExecute(NULL, "open", "UpdateEmulator.bat", NULL, NULL, SW_SHOWMINIMIZED);
