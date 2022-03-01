ff1cd21309ec78cf044e6b1e02cd225a5c3008fa
Nora <norahanegan@gmail.com>
2022-02-28 21:35:32 -0500

    rebase to new codebase

--- Source/Project64-core/N64System/N64Class.h
+++ Source/Project64-core/N64System/N64Class.h
@@ -10,8 +10,6 @@
 ****************************************************************************/
 #pragma once
 
-#define USE_DISCORD //undef this to build without Discord support
-
 #include <Common/SyncEvent.h>
 #include <Common/Thread.h>
 #include <Project64-core/Settings/N64SystemSettings.h>
@@ -30,11 +28,6 @@
 #include "CheatClass.h"
 #include "FramePerSecondClass.h"
 #include "SpeedLimiterClass.h"
-
-#ifdef USE_DISCORD
-#include <Project64-core\3rdParty\mario_party_netplay.h>
-#include <Project64-core\3rdParty\discord-rpc.h>
-#endif
 
 typedef std::list<SystemEvent>   EVENT_LIST;
 
@@ -188,26 +181,4 @@ private:
 
     //list of function that have been called .. used in profiling
     FUNC_CALLS m_FunctionCalls;
-
-	//Mario Party Netplay
-	//Has a state already been auto-saved?
-	bool m_HasAutosaved;
-    
-    //Discord RPS functions and variables
-#ifdef USE_DISCORD
-    char*   m_DiscordApplicationId;
-    uint8_t m_DiscordCurrentPlayers;
-    int64_t m_DiscordNextPost;
-    bool    m_DiscordSendPresence;
-    int64_t m_DiscordStartTime;
-
-    void    discordInit();
-    void    discordUpdate();
-    void    getMk64Rps(uint8_t* Rdram, DiscordRichPresence& discordPresence);
-    void    getMp1Rps(uint8_t* Rdram, DiscordRichPresence& discordPresence);
-    void    getMp2Rps(uint8_t* Rdram, DiscordRichPresence& discordPresence);
-    void    getMp3Rps(uint8_t* Rdram, DiscordRichPresence& discordPresence);
-    void    getSsbRps(uint8_t* Rdram, DiscordRichPresence& discordPresence);
-    void    getNumberControllers();
-#endif
 };
